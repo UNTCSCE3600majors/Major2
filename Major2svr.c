ef18ff762dfdffed4f0d4b7758d0ac87d70f9158
paintproeli <33049894+paintproeli@users.noreply.github.com>
2017-11-30 14:35:47 -0600

    Add files via upload

---
+++ Major2svr.c
@@ -0,0 +1,11 @@
+// Eli Cruse, Jared Westmoreland, Dan Shadel, Logan-kun
+
+
+// Things he said in class
+// use threads instead of the select function
+// 2 static variables to keep track of the totals
+
+/* recommendation from charles:
+He recommends using a link list to store each client since we can only pass
+1 parameter to the threads, we would just pass the head of the linked list */
+
